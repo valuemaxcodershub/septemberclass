94dc5a0a687ff63f93b4bdccb03546a8be8ecdd5
Tunji <valuemaxcommunications@gmail.com>
2026-09-03 09:39:44 +0100

    i added script

---
+++ script.js
@@ -0,0 +1,27 @@
+        // "use strict";
+		// 	let userName = prompt("what is your name?");
+		// 	let age = prompt("what is your age?");
+		// 	alert("Hello " + userName + " you are " + age + " years old");
+			 
+
+        // let userPrice = 100;
+        // if(true){
+        //     let userPrice = 50;
+        // }
+        // console.log(userPrice);
+        // var userPrice = 100;
+        // if(true){
+        //     var userPrice = 50;
+        // }
+        // console.log(userPrice);
+        // const dateofBirth = 2000_04_01;
+        // console.log(dateofBirth);
+        // let userMonth = "october";
+        // userMonth = "november";
+        // console.log(userMonth);
+        // let userAge = 20;
+        // userAge = 30;
+        // alert("Hello you are " + userAge + " years old");
+        let userName = "samad";
+        userName = "ike";
+            alert("Hello " + userName + "!");
